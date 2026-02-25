e55df55a31fac908588495dcda986682fc641a06
IanMartinezPicazo <iama249@vidalibarraquer.net>
2026-02-25 10:26:51 +0100

    Event detail activity design

---
+++ app/src/main/java/com/example/ttgneventos/model/EventDetails.java
@@ -0,0 +1,31 @@
+package com.example.ttgneventos.model;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.ttgneventos.R;
+
+public final class EventDetails extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_event_details);
+        ViewCompat.setOnApplyWindowInsetsListener
+        (
+            findViewById(R.id.main), (v, insets) ->
+            {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            }
+        );
+    }
+}
